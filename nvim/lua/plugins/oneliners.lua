138aa29e56b45cc04391322f4d197f71e1df10c4
5vrs <222455745+5vrs@users.noreply.github.com>
2025-10-15 21:40:44 +0100

    format comments

--- nvim/lua/plugins/oneliners.lua
+++ nvim/lua/plugins/oneliners.lua
@@ -1,11 +1,11 @@
 return {
-   { -- Show CSS Colors
+   { -- show css colors
         'brenoprata10/nvim-highlight-colors',
         config = function()
             require('nvim-highlight-colors').setup({})
         end
     },
-    { -- Git plugin
+    { -- git plugin
         'tpope/vim-fugitive',
     },
 }
